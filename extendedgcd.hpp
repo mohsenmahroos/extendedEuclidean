a653939e6d656bac40d518c31000db805eb450d1
mohsenmahroos <mohsenmahroos@msn.com>
2022-08-03 17:41:59 +0200

    Update extendedgcd.hpp

--- extendedgcd.hpp
+++ extendedgcd.hpp
@@ -1,11 +1,12 @@
 #pragma once
 #include <iostream>
 #include <tuple>
- 
+
 template<typename int_t>
 auto extended_gcd(int_t a, int_t b) {
+    using itriple = std::tuple<int_t,int_t,int_t>;
     if (a == 0)
-    	return std::tuple<int_t,int_t,int_t>(b,0,1);
+        return itriple(b,0,1);
     const auto quotient = b/a, remainder = b%a;
     const auto [gcd,x,y] = extended_gcd(remainder,a);
-    return std::tuple<int_t,int_t,int_t>(gcd,y-quotient*x,x); }
+    return itriple(gcd,y-quotient*x,x); }
